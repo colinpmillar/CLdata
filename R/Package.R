d47b71afdcadc48254e5503cf02b53609aacd548
colinpmillar <colinpmillar@gmail.com>
2015-01-13 10:00:37 +0000

    R files for packages and datasets

---
+++ R/Package.R
@@ -0,0 +1,7 @@
+#' Data for the Estimation of Salmonid Abundance in Scottish Rivers
+#'
+#' @docType package
+#' @name CLdata
+#' @import sp
+#'
+NULL
---
+++ R/data.R
@@ -0,0 +1,88 @@
+#' Electrofishing data.
+#'
+#'
+#' This data set is .
+#'
+#' @docType data
+#' @name ef
+#' @usage ef
+#' @format A data.frame
+#' @keywords datasets
+'ef'
+
+
+#' GIS covariates spatial points data.
+#'
+#'
+#' This data set is .
+#'
+#' @docType data
+#' @name gis
+#' @usage gis
+#' @format A spatial points data.frame
+#' @keywords datasets
+'gis'
+
+
+#' Hydrometric area data.
+#'
+#'
+#' This data set is .
+#'
+#' @docType data
+#' @name hma
+#' @usage hma
+#' @format A spatial polygons data.frame
+#' @keywords datasets
+'hma'
+
+#' Rivers spatial lines data.
+#'
+#'
+#' This data set is .
+#'
+#' @docType data
+#' @name newrivs
+#' @usage newrivs
+#' @format A spatial lines data.frame
+#' @keywords datasets
+'newrivs'
+
+
+#' A list of common projections
+#'
+#'
+#' This data set is .
+#'
+#' @docType data
+#' @name proj
+#' @usage proj
+#' @format A list
+#' @keywords datasets
+'proj'
+
+
+#' Catchment spatial polygons data.
+#'
+#'
+#' This data set is .
+#'
+#' @docType data
+#' @name redctm
+#' @usage redctm
+#' @format A spatial polygons data.frame
+#' @keywords datasets
+'redctm'
+
+
+#' Rivers spatial lines data.
+#'
+#'
+#' This data set is .
+#'
+#' @docType data
+#' @name redrivs
+#' @usage redrivs
+#' @format A spatial lines data.frame
+#' @keywords datasets
+'redrivs'
